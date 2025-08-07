416b9447253e59a661eadc8b61be48408cc8514e
onercede99 <letrunghieu15071998@gmail.com>
2025-08-07 15:38:24 +0700

    Sửa đường dẫn trang adminDashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,52 @@
-// src/App.jsx
 import React from "react";
 
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import Register from './components/login-register/Register';
-import Login from './components/login-register/Login';
-import UserProfilePage from './pages/UserProfilePage.jsx';
-import ForgotPassword from './components/login-register/ForgotPassword';
-import AdminLogin from './components/admin/AdminLogin.jsx';
+import Register from "./components/login-register/Register";
+import Login from "./components/login-register/Login";
+import UserProfilePage from "./pages/UserProfilePage.jsx";
+import ForgotPassword from "./components/login-register/ForgotPassword";
+import AdminLogin from "./components/admin/AdminLogin.jsx";
+import AdminPage from "./pages/AdminPage";
+import AdminRoute from "./components/admin/AdminRoute";
 
 // Protected Route Component
 const ProtectedRoute = ({ children }) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   return token ? children : <Navigate to="/login" replace />;
 };
-
-
 
 function App() {
   return (
     <Router>
       <Routes>
-
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
 
-          {/* Protected Routes */}
-        <Route 
-          path="/profile" 
+        <Route
+          path="/profile"
           element={
             <ProtectedRoute>
               <UserProfilePage />
             </ProtectedRoute>
-          } 
+          }
         />
-        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route
+          path="/admin/*"
+          element={
+            <AdminRoute>
+              <AdminPage />
+            </AdminRoute>
+          }
+        />
       </Routes>
     </Router>
   );
